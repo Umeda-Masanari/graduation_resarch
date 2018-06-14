544aefc2a3c43de3a8a2ab5da7b798c6eb54d6a6
Umeda Masanari <m14030@g.metro-cit.ac.jp>
2018-06-15 00:12:04 +0900

    とりあえず完成

--- fading-time.c
+++ fading-time.c
@@ -3,19 +3,29 @@
 #include <math.h>
 #include "complex.h"
 
-#define WLT　64　//素波の数64–128の間くらい
+#define WLT 64 //素波の数64–128の間くらい
 #define dt 0.003 //測定の間隔[s]
 #define Freq 2.5e9 //2.5[GHz]帯の周波数
-
+#define V (4.5e3 / 3600)
 int main(void){
-  double *I, *Q, *theta;
+  double *I, *Q, *theta, g, k;
+  double t;
   //double N_factor =  //N_factor=正規化係数 は自分で見つける
 
-  double lambda = 299792458/Freq;//真空中の搬送波の波長/周波数　
+  double lambda = 3.0e8/Freq;//真空中の搬送波の波長/周波数　
   double f_D= V/lambda; //最大ドップラー周波数
   int i;
   for (i = 0; i < WLT; i++) {
+    k= f_D*cos(2.0*M_PI*(double)i/(double)WLT);
+    g=2.0 * M_PI * k*(double)dt*(double)t;
 
-  }
+double t = theta[i];
+
+printf("%d\t%f\n",i,t );
+  *I+= cos(g+theta[i]);
+  *Q+= sin(g+theta[i]);
+}
+
+return 0;
 
 }
